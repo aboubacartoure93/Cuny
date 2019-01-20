e586730710b052ab786e0f92c539818b46ff5af4
aboubacar toure <aboubacartoure93@gmail.com>
2019-01-20 02:58:49 -0500

    chatbox working for simple chat and chat for book.... need to work chat design now

--- app/assets/javascripts/books.js
+++ app/assets/javascripts/books.js
@@ -18,16 +18,16 @@ var ready = function () {
     });
 
 
-    // $('.start-conversationBook').click(function (e) {
-    //     e.preventDefault();
+   $('.start-conversationSimple').click(function (e) {
+        e.preventDefault();
 
-    //     var sender_id = $(this).data('sid');
-    //     var recipient_id = $(this).data('rip');
+        var sender_id = $(this).data('sid');
+        var recipient_id = $(this).data('rip');
 
-    //     $.post("/conversations", { sender_id: sender_id, recipient_id: recipient_id }, function (data) {
-    //         chatBox.chatWith(data.conversation_id);
-    //     });
-    // });
+        $.post("/conversations", { sender_id: sender_id, recipient_id: recipient_id }, function (data) {
+            chatBoxS.chatWith(data.conversation_id);
+        });
+    });
 
 
 
