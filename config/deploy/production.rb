f42a79c603eda8367cecea9711c62b1775724c62
aboubacar toure <aboubacartoure93@gmail.com>
2019-02-15 07:02:57 -0500

    try to deploy on digitaocean

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,3 +1,7 @@
+#set :stage, :production
+
+server '142.93.246.213', user: 'deploy', roles: %w{app db web}
+
 # server-based syntax
 # ======================
 # Defines a single server with a list of roles and multiple properties.
